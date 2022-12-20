f9c6afe14315516fcf29006345a19bf36a4019b4
JIHYE <jihyem75@gmail.com>
2022-12-20 10:17:35 +0900

    :bulb: Navigation locations and default scan destinations

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+@ComponentScan(
+    excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))    // default 일때 config 파일의 위치부터 하위의 모든 파일을 스캔한다
 public class AutoAppConfig {
 }
