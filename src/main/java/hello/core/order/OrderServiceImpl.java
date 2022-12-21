3240346113af5428d0e29fa6c39ef339e728d88e
JIHYE <jihyem75@gmail.com>
2022-12-21 14:38:32 +0900

    :white_check_mark: More than 2 lookup beans - problem

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
@@ -16,12 +15,12 @@ public class OrderServiceImpl implements OrderService {
     // 수정자 주입을 포함한 나머지 주입 방식은 모두 생상자 이후에 호출되므로, 필드에 final 키워드를 사용할 수 없다.
     // 오직 생성자 주입 방식만 final 키워드를 사용할 수 있다
 
-    //@Autowired  // 생성자가 하나라면 생략가능
-//    public OrderServiceImpl(MemberRepository memberRepository,
-//                            DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired  // 생성자가 하나라면 생략가능
+    public OrderServiceImpl(MemberRepository memberRepository,
+                            DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
 //    @Autowired
 //    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
