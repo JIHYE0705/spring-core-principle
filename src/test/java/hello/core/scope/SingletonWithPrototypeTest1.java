b5f371d188e2b192f277f29dc15f7a0e5676b54f
JIHYE <jihyem75@gmail.com>
2022-12-23 10:43:07 +0900

    :white_check_mark: Prototype Scope - Provider solves problems when used with singleton beans

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -3,8 +3,11 @@ package hello.core.scope;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -35,35 +38,29 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton") // default 라 생략 가능
     static class ClientBean {
-        private final PrototypeBean prototypeBean;  // 생성시점에 주입 x01
+//         private final Provider<PrototypeBean> prototypeBeanProvider; // Provider 를 사용하면 빈을 주입받지 못하는 에러가 나는데 해결 못함...
+//
+//        ClientBean(Provider<PrototypeBean> prototypeBeanProvider) {
+//            this.prototypeBeanProvider = prototypeBeanProvider;
+//        }
+         private final ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
+        ClientBean(ObjectProvider<PrototypeBean> prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
         }
+        // private ObjectFactory<PrototypeBean> prototypeBeanProvider;  // 과거에는 ObjectFactory 가 있었는데, 여기에 편의 기능을 추가해서 ObjectProvider 가 만들어졌다
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            //PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
-
-    }    @Scope("singleton") // default 라 생략 가능
-    static class ClientBean2 {
-        private final PrototypeBean prototypeBean;  // 생성시점에 주입 x02
-
-        public ClientBean2(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
-
-        public int logic() {
-            prototypeBean.addCount();
-            return prototypeBean.getCount();
-        }
-
     }
 
     @Scope("prototype")
